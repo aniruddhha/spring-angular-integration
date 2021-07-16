9962c2442b079df32ce4968f7ddeb832b3983f1d
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-16 16:54:49 +0530

    port changed

--- src/main/java/com/ani/all/controller/IotDeviceController.java
+++ src/main/java/com/ani/all/controller/IotDeviceController.java
@@ -5,6 +5,10 @@ import com.ani.all.dto.AppRes;
 import com.ani.all.dto.IotDeviceDto;
 import com.ani.all.service.IotDeviceService;
 import com.ani.all.util.AppResUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
+@Api("Managing Iot Devices")
 @AllArgsConstructor
 @RequestMapping(value = "/device")
 @RestController
@@ -19,6 +25,12 @@ public class IotDeviceController {
 
     private final IotDeviceService service;
 
+    @ApiOperation(value = "It adds iot devices")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "successfully added"),
+            @ApiResponse(code = 400, message = "UnAuthorized access"),
+            @ApiResponse(code = 404, message = "Not Found")
+    })
     @PostMapping
     public ResponseEntity<AppRes<IotDeviceDto>> save(@RequestBody IotDevice device) {
 
@@ -34,6 +46,7 @@ public class IotDeviceController {
         );
     }
 
+    @ApiOperation("it lists all registered devices")
     @GetMapping
     public ResponseEntity<AppRes<List<IotDeviceDto>>> devices() {
 
